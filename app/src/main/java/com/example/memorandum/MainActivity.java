8e1807dcfcc161cc920a7e49cefcdd77e83b6818
ArisatoX <patricksungkharisma27@gmail.com>
2020-04-16 22:09:55 +0700

    update toolbar

--- app/src/main/java/com/example/memorandum/MainActivity.java
+++ app/src/main/java/com/example/memorandum/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.memorandum;
 
 import android.os.Bundle;
-import android.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -13,16 +12,29 @@ public class MainActivity extends AppCompatActivity {
     //Variables
     DrawerLayout drawerLayout;
     NavigationView navigationView;
-    Toolbar toolbar;
+//    Toolbar toolbar;
 
-    //Init
+    //Main
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    //Hooks
+        //Hooks
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+//        toolbar = findViewById(R.id.toolbar);
+
+        //Toolbar
+
+//
+//        //Navigation Drawer Menu
+//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawer_open,R.string.drawer_close);
+//        drawerLayout.addDrawerListener(toggle);
+//        toggle.syncState();
+
+
+    }
 
 
 }
