d865b4a9745604512c08e9bdda7fc63d078d3070
ArisatoX <patricksungkharisma27@gmail.com>
2020-04-16 23:36:10 +0700

    clickable menu

--- app/src/main/java/com/example/memorandum/MainActivity.java
+++ app/src/main/java/com/example/memorandum/MainActivity.java
@@ -1,15 +1,18 @@
 package com.example.memorandum;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     //Variables
     DrawerLayout drawerLayout;
@@ -29,15 +32,35 @@ public class MainActivity extends AppCompatActivity {
 
         //Toolbar
         setSupportActionBar(toolbar);
+        setTitle("");
 
         //Navigation Drawer Menu
+        navigationView.bringToFront();
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawer_open,R.string.drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
+        navigationView.setNavigationItemSelectedListener(this);
 
     }
 
+    //Click back -> close drawer
+    @Override
+    public void onBackPressed() {
+
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+
+    //To click menu items
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return true;
+    }
 
 }
 
