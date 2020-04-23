691deb9d092e6ae33285d0f5a3a830907ed489a2
ArisatoX <patricksungkharisma27@gmail.com>
2020-04-24 02:20:11 +0700

    datepicker added schedule

--- app/src/main/java/com/example/memorandum/ScheduleActivity.java
+++ app/src/main/java/com/example/memorandum/ScheduleActivity.java
@@ -1,8 +1,13 @@
 package com.example.memorandum;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -10,15 +15,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.DialogFragment;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class ScheduleActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class ScheduleActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, DatePickerDialog.OnDateSetListener {
 
     //Variables
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Toolbar toolbar;
+    Button dateButton;
+    TextView dateText;
 
     //Main
     @Override
@@ -30,6 +41,8 @@ public class ScheduleActivity extends AppCompatActivity implements NavigationVie
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.nav_view);
         toolbar = findViewById(R.id.toolbar);
+        dateButton = findViewById(R.id.dateButton);
+        dateText = findViewById(R.id.dateText);
 
         //Toolbar
         setSupportActionBar(toolbar);
@@ -48,6 +61,15 @@ public class ScheduleActivity extends AppCompatActivity implements NavigationVie
         navigationView.setNavigationItemSelectedListener(this);
 
         navigationView.setCheckedItem(R.id.Schedule);
+
+        //Calendar Listener Settings
+        dateButton.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick (View v){
+                DialogFragment datePicker = new DatePickerFragment();
+                datePicker.show(getSupportFragmentManager(), "Date Picker");
+            }
+        });
     }
 
     //Click back -> close drawer
@@ -85,6 +107,22 @@ public class ScheduleActivity extends AppCompatActivity implements NavigationVie
 
         return true;
     }
+
+    //Set Date Calendar settings
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int day)
+    {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, year);
+        c.set(Calendar.MONTH, month);
+        c.set(Calendar.DAY_OF_MONTH, day);
+        String currentDateString = DateFormat.getDateInstance().format(c.getTime());
+        dateText.setText(currentDateString);
+    }
+
+
+
+
 
 }
 
