535af88ef4f75090bb300d7d81ec827c2547ab71
ArisatoX <patricksungkharisma27@gmail.com>
2020-05-07 21:46:57 +0700

    read layout improved

--- app/src/main/java/com/example/memorandum/NotesRead.java
+++ app/src/main/java/com/example/memorandum/NotesRead.java
@@ -2,6 +2,8 @@ package com.example.memorandum;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +13,7 @@ public class NotesRead extends AppCompatActivity {
     //Variables
     TextView title;
     TextView content;
+    Button noteBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +23,20 @@ public class NotesRead extends AppCompatActivity {
         //Hooks
         title = findViewById(R.id.noteReadTitle);
         content = findViewById(R.id.noteReadContent);
+        noteBack = findViewById(R.id.noteBack);
 
+        //Set Text
         Intent intent = getIntent();
         title.setText(intent.getStringExtra("title"));
         content.setText(intent.getStringExtra("content"));
+
+        // Back Button
+        noteBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+                Intent intent = new Intent(NotesRead.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
